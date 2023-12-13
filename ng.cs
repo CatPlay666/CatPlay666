16890b172a2381c091bb84a8ba701a05ad53ba35
CatPlay666 <153101571+CatPlay666@users.noreply.github.com>
2023-12-13 18:29:56 +0500

    ДЗ АЛГОРИТМИКА

---
+++ ng.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ng : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //щрн йнлюмдю охьхр б йнмянке опхбер лхххххххххп!!!ннюнюнюнюнюннюнюнюнюнюнюннюнюню
+        print("я МНБШЛ ЦНДНЛ!! 2024!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//йНМЕЖ ЯЙПХОРЮ :P
